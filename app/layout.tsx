8d954564ff546ac106f305abb463173779170f5c
mayurathavale18 <mayurathavale.code100@protonmail.com>
2024-11-26 02:05:47 +0530

    Done with Layout.tsx to update the left side of the auth page. Next: Reusable AuthForm

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,18 @@
 import type { Metadata } from "next";
-import { Poppins } from 'next/font/google';
+import { Poppins } from "next/font/google";
 import "./globals.css";
+import React from "react";
 
 const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   variable: "--font-poppins",
 });
 
 export const metadata: Metadata = {
   title: "StoreIt",
   description: "StoreIt - The only storage app you ever need",
-};  
+};
 
 export default function RootLayout({
   children,
@@ -20,11 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${poppins.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${poppins.variable} antialiased`}>{children}</body>
     </html>
   );
 }
