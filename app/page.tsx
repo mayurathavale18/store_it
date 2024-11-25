040a9196bdb8a3f3921b954216d13bb9d85474ca
mayurathavale18 <mayurathavale.code100@protonmail.com>
2024-11-25 17:02:33 +0530

    Done with Design System, Theming & Tailwind config . Next: Auth Layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <div className="text-3xl">
-      StoreIt : The only storage solution you need!
+    <div className="flex-center h-screen bg-amber-100">
+      <h1 className="text-3xl font-bold text-brand">
+        StoreIt : The only storage solution you need!
+      </h1>
     </div>
   );
 }
