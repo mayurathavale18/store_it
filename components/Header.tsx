c2681fc9fd1262375926b135a58df26b138eb54d
mayurathavale18 <mayurathavale.code100@protonmail.com>
2024-11-30 03:27:10 +0530

    Done with Mobile Nav-bar UI. Next: Finalizing Auth processes ( SignIn and SignOut )

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,14 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Search from "@/components/Search";
+import FileUploader from "@/components/FileUploader";
 
 const Header = () => {
   return (
     <header className="header">
-      Search
+      <Search />
       <div className="header-wrapper">
-        FileUploader
+        <FileUploader />
         <form>
           <Button type="submit" className="sign-out-button">
             <Image
--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -1,4 +1,121 @@
-const MobileNavigation = () => {
-  return <div>MobileNavigation</div>;
+"use client";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Image from "next/image";
+import { useState } from "react";
+import { usePathname } from "next/navigation";
+import { SeparatorHorizontal } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
+import { navItems } from "@/constants";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+
+interface Props {
+  email: string;
+  fullName: string;
+  avatar: string;
+  ownerId: string;
+  accountId: string;
+}
+
+const MobileNavigation = ({
+  ownerId,
+  accountd,
+  email,
+  fullName,
+  avatar,
+}: Props) => {
+  const [open, setOpen] = useState(false);
+  const pathname = usePathname();
+
+  return (
+    <header className="mobile-header">
+      <Image
+        src="/assets/icons/logo-full-brand.svg"
+        alt="logo"
+        width={120}
+        height={52}
+        className="h-auto"
+      />
+      <Sheet open={open} onOpenChange={setOpen}>
+        <SheetTrigger>
+          <Image
+            src="assets/icons/menu.svg"
+            alt="Search"
+            width={30}
+            height={30}
+          />
+        </SheetTrigger>
+        <SheetContent className="shad-sheet flex h-screen flex-col px-3">
+          <SheetTitle>
+            <div className="header-user">
+              <Image
+                src={avatar}
+                alt="avatar"
+                width={44}
+                height={44}
+                className="header-user-avatar"
+              />
+              <div className="sm:hidden lg:block">
+                <p className="subtitle-2 capitalize">{fullName}</p>
+                <p className="caption">{email}</p>
+              </div>
+            </div>
+            <Separator className="mb-4 bg-light-200/20" />
+          </SheetTitle>
+
+          <nav className="mobile-nav">
+            <ul className="mobile-nav-list">
+              {navItems.map(({ url, name, icon }) => (
+                <Link key={name} href={url} className="lg:w-full">
+                  <li
+                    className={cn(
+                      "mobile-nav-item",
+                      pathname === url && "shad-active",
+                    )}
+                  >
+                    <Image
+                      src={icon}
+                      alt={name}
+                      width={50}
+                      height={24}
+                      className={cn(
+                        "nav-icon",
+                        pathname === url && "nav-icon-active",
+                      )}
+                    />
+                    <p>{name}</p>
+                  </li>
+                </Link>
+              ))}
+            </ul>
+          </nav>
+          <Separator className="my-5 bg-light-200/20" />
+          <div className="flex flex-1 flex-col justify-between gap-5">
+            FileUploader
+            <Button
+              type="submit"
+              className="sign-out-button"
+              onClick={() => {}}
+            >
+              <Image
+                src="/assets/icons/logout.svg"
+                alt="logout"
+                width={24}
+                height={24}
+              />
+            </Button>
+          </div>
+        </SheetContent>
+      </Sheet>
+    </header>
+  );
 };
 export default MobileNavigation;
